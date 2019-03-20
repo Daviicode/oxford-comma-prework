cb95ca9bd0cc2c1e0a45348b630c73257395a7db
David zhang <d.ydzdavid@yahoo.com>
2019-03-20 07:01:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+  puts array
+  else if array.length == 2 
+    puts array[0] + " and " + array[1]
+  else
+    array.each do |num|
+      puts array[0] + " and " + array[-1]
+    end
 end
