1f2ad172797196f08288a5804caef02e4b5786aa
David zhang <d.ydzdavid@yahoo.com>
2019-03-20 07:34:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,24 +2,13 @@ def oxford_comma(array)
   if array.length == 1 
     return "#{array[0]}"
   
-  else if array.length == 2 
+  elsif array.length == 2 
     return array.join(" and ")
     
-  else if array.length >= 3
+  elsif array.length >= 3
     array[-1] = " and #{array[-1]}."
     return array.join(", ")
     
     end
 end
 
-
-def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
-  end
-end
