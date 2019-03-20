ce07df1e2c99dded24f5b5064655d11a22ef498b
David zhang <d.ydzdavid@yahoo.com>
2019-03-20 07:29:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   return "#{array[0]}"
   else if array.length == 2 
     return array.join(" and ")
-  else if
+  else if array.length >= 3
     array[-1] = " and #{array[-1]}."
     return array.join(", ")
     end
